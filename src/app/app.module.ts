65d030f82d29512de2151dedcd43c5a091313a72
Ravi Shrivastava <rsravishri30@gmail.com>
2018-04-14 14:55:52 +0530

    added Angular for with submit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/course/course.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent  {
+  @Input('title') title: string;
+
+  categories = [
+    { id: 1, name: 'Development' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' },
+  ];
+
+  onSubmit(values){
+  	console.log(values);
+  }
+}
