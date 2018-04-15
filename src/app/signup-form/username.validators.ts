2086453d938b6e2f6ab4f4967fed56e8282bef37
Ravi Shrivastava <rsravishri30@gmail.com>
2018-04-15 14:55:34 +0530

    added async custom validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,4 +1,5 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
+
 export class UsernameValidators {
 
 	static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
@@ -8,4 +9,14 @@ export class UsernameValidators {
 		return null;
 	}
 
+	static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null > {
+		return new Promise((resolve,reject) => {
+			setTimeout(() => {
+				if(control.value === 'mosh')
+					resolve( { shouldBeUnique: true } );
+				else
+					resolve(null);
+			},2000);
+		});
+	}
 }
