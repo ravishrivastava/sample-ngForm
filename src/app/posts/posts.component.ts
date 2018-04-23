f6230a562c871a09cb5da45872dfc95ab8bbc0ae
Ravi Shrivastava <rsravishri30@gmail.com>
2018-04-23 05:53:46 +0530

    implemented post with basic http patch and delete

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -29,6 +29,22 @@ export class PostsComponent {
 
   }
 
+  updatePost(post){
+    this.http.patch(this.url + '/'+ post.id,JSON.stringify({ isRead: true}))
+    .subscribe(response => {
+      console.log(response);
+    });
+  }
+  deletePost(post){
+    this.http.delete(this.url + '/'+ post.id)
+    .subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index,1)
+      console.log(response);
+    });
+  }
+  
+
   
 
 }
