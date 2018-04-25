fd1fb76e6000d7e526541d3c1b145f8a1fbf331f
Ravi Shrivastava <rsravishri30@gmail.com>
2018-04-25 23:13:27 +0530

    extracting reusable data service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,7 +16,7 @@ export class PostsComponent implements OnInit {
   }
   
   ngOnInit(){
-    this.service.getPosts()
+    this.service.getAll()
     .subscribe(response  => {
       this.posts = response.json();
     });
@@ -24,7 +24,7 @@ export class PostsComponent implements OnInit {
   createPost(input: HTMLInputElement){
     let post = {title: input.value };
     input.value = '';
-    this.service.createPost(post)
+    this.service.create(post)
     .subscribe(response => {
       post['id'] = response.json().id;
       this.posts.splice(0,0,post)
@@ -39,14 +39,14 @@ export class PostsComponent implements OnInit {
   }
 
   updatePost(post){
-    this.service.updatePost(post)
+    this.service.update(post)
     .subscribe(response => {
       console.log(response);
     });
   }
 
   deletePost(post){
-    this.service.deletePost(post.id)
+    this.service.delete(post.id)
     .subscribe(response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index,1)
